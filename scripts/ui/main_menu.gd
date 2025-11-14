314412cd372677b0610e323efa5514dd19af7da2
Derin Aşır <derinasir@gmail.com>
2025-11-14 21:41:20 +0300

    Remove support from playable character Maria

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -6,10 +6,5 @@ func _on_castle_guard_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/castle_guard_test_scene.tscn")
 
 
-
-func _on_maria_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/maria_test_scene.tscn")
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
