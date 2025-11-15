a562f62964333ba1b86036e8f84efdaf0b8d531f
Derin Aşır <derinasir@gmail.com>
2025-11-15 21:06:14 +0300

    Add SFX to pumpkin smash

--- scripts/smashable_objects/model_breaker.gd
+++ scripts/smashable_objects/model_breaker.gd
@@ -1,13 +1,15 @@
 extends Node3D
 
-
 const INTENSITY: float = 6.0
 
 func _ready() -> void:
 	for i in get_child_count():
+		if get_child(i) is AudioStreamPlayer3D:
+			continue
 		var angle: float = TAU * i / get_child_count()
 		var dir = Vector3(cos(angle), 0, sin(angle)).normalized()
 		get_child(i).apply_central_impulse(dir * INTENSITY)
 
+	$AudioStreamPlayer3D.play(0.25)
 	await get_tree().create_timer(5).timeout
 	queue_free()
