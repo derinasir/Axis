4cbe7431e7d245e60bbdb96ab5c839f89b184ad7
Derin Aşır <derinasir@gmail.com>
2025-11-15 20:50:11 +0300

    Add hit detection to pumpkin

--- scripts/smashable_objects/model_replacer.gd
+++ scripts/smashable_objects/model_replacer.gd
@@ -3,10 +3,13 @@ extends Node3D
 @export var PIECES: PackedScene
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		var pieces: Node3D = PIECES.instantiate()
-		get_parent().add_child(pieces)
-		pieces.transform = transform
-		
-		queue_free()
+func replace_model() -> void:
+	var pieces: Node3D = PIECES.instantiate()
+	get_parent().add_child(pieces)
+	pieces.transform = transform
+	
+	queue_free()
+
+
+func _on_combat_manager_died(_damage_data: DamageData) -> void:
+	replace_model()
